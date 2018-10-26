0ed2a2efbc6d5ce5b615e7e3be55152cf2ab38b9
MarcusVsc1 <37600192+MarcusVsc1@users.noreply.github.com>
2018-10-26 20:34:51 -0300

    Exercício 14 - Setor Pessoal e Funcionários

--- exercicio14/Exercicio14.java
+++ exercicio14/Exercicio14.java
@@ -9,7 +9,12 @@ public class Exercicio14 {
 
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        SetorPessoal rh = new SetorPessoal();
+        rh.adicionaFuncionario(new Funcionario(201776013,"Joseph Stalin",5,4860,"Comunista"));
+        rh.adicionaFuncionario(new Funcionario(201796013,"Mister M",3,20587,"Ilusionista"));
+        rh.adicionaFuncionario(new Funcionario(201796913,"Lenny Face",1,15832,"Almoxarife"));
+        rh.imprimeFolha();
+        rh.imprimeFolhaCrescente();
     }
     
 }
--- exercicio14/SetorPessoal.java
+++ exercicio14/SetorPessoal.java
@@ -15,5 +15,82 @@ import java.util.ArrayList;
  * @author ice
  */
 public class SetorPessoal {
-    ArrayList<Funcionario> funcionarios;
+    private ArrayList<Funcionario> funcionarios;
+
+    public SetorPessoal() {
+        funcionarios = new ArrayList();
+    }
+    
+    
+    
+    public void adicionaFuncionario(Funcionario funcionario){
+        funcionarios.add(funcionario);
+    }
+    
+    public void imprimeFolha(){
+        System.out.println("Segue folha de pagamento:");
+        for (Funcionario funcionario : funcionarios) {
+            System.out.println(funcionario.getNome()+": "+funcionario.getSalario());
+        }   
+        
+    }
+    
+    public float totalFolha(){
+        float folha = 0;
+        for (Funcionario funcionario : funcionarios){
+            folha+=funcionario.getSalario();
+        }
+        return folha;
+    }
+    
+    public String maiorSalario(){
+        int maior = 0;
+        float salarioMaior = 0;
+        for(int i = 0; i< funcionarios.size(); i++){
+            if(funcionarios.get(i).getSalario()>salarioMaior){
+                maior = i;
+                salarioMaior = funcionarios.get(i).getSalario();
+            }
+        }
+        return funcionarios.get(maior).getNome();
+    }
+    
+    public void imprimeDepto(int depto){
+        System.out.println("Segue os funcionarios do departamento "+depto);
+        for(Funcionario funcionario : funcionarios){
+            if(funcionario.getDepartamento()==depto){
+                System.out.println(funcionario.getNome()+": Salario de"+funcionario.getSalario());
+            }
+        }
+    }
+    
+    public void imprimeFuncao(String funcao){
+        System.out.println("Segue a lista de funcionarios cuja função é "+funcao);
+        for(Funcionario funcionario : funcionarios){
+            if(funcionario.getFuncao().equals(funcao)){
+                System.out.println(funcionario.getNome());
+            }
+        }
+    }
+    
+    public void imprimeFolhaCrescente(){
+        ArrayList<Funcionario> aux = new ArrayList();
+        
+        for (Funcionario funcionario : funcionarios) {
+            for (int i = 0; i<aux.size();i++) {
+                 if(funcionario.getSalario()>aux.get(i).getSalario()){
+                     aux.add(i, funcionario);
+                     break;
+                 }
+            }
+            if(!aux.contains(funcionario)){
+                aux.add(funcionario);
+            }
+        }
+        
+        System.out.println("Segue a lista de funcionarios por ordem crescente de salario.");
+        for(Funcionario funcionario : aux){
+            System.out.println(funcionario.getNome()+" - Salario: "+funcionario.getSalario());
+        }
+    }
 }
